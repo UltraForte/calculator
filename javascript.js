80f864c656bda1f4a5688dfe1cb46e2fc846d6a9
UltraForte <ultraforte777@gmail.com>
2024-09-13 13:28:42 -0400

    Link CSS and JS to index.html
    And declare main variables and write main functions.

---
+++ javascript.js
@@ -0,0 +1,31 @@
+let inputOne
+let inputTwo
+let operator
+
+function add(inputOne, inputTwo) {
+    return (inputOne + inputTwo);
+};
+let sum = add(5,2);
+console.log("I added:" + sum);
+
+
+function subtract(inputOne, inputTwo) {
+    return (inputOne - inputTwo);
+}
+let difference = subtract(5,2);
+console.log("I subtracted:" + difference);
+
+
+function multiply(inputOne, inputTwo) {
+    return (inputOne * inputTwo);
+};
+let product = multiply(5,2);
+console.log("I multiplied:" + product);
+
+
+function divide(inputOne, inputTwo) {
+    return (inputOne / inputTwo);
+};
+let quotient = divide(5,2);
+console.log("I divided:" + quotient);
+
