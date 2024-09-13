86d551edd5775d093e540aabf5e391c8c9559956
UltraForte <ultraforte777@gmail.com>
2024-09-13 13:50:42 -0400

    Add the "operate" function
    This function executes the correct operation depending
    on the "operand" value.

--- javascript.js
+++ javascript.js
@@ -29,3 +29,21 @@ function divide(inputOne, inputTwo) {
 let quotient = divide(5,2);
 console.log("I divided:" + quotient);
 
+inputOne = 10;
+inputTwo = 5;
+operator = ("+");
+function operate(inputOne, inputTwo, operator) {
+    let execution
+    if (operator === "+") {
+        return add(inputOne, inputTwo);
+    } else if (operator === "-") {
+        return subtract(inputOne, inputTwo);
+    } else if (operator === "*") {
+        return multiply(inputOne, inputTwo);
+    } else if (operator === "/") {
+        return divide(inputOne, inputTwo);
+    };
+
+};
+let result = operate(inputOne, inputTwo, operator);
+console.log(result);
