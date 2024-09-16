d93cd9ea825f08246d3df9041e25f5f2440133d5
UltraForte <ultraforte777@gmail.com>
2024-09-16 14:41:53 -0400

    Create the display input, and fix class typos.
    The display input is also functional(except "="), you can input characters
    using the UI buttons. Fix a typo in class of button "0", and in
    class of the "Divide" button, as well as replace "plus" with "add"
    in the class of "Add" button.

--- javascript.js
+++ javascript.js
@@ -43,7 +43,77 @@ function operate(inputOne, inputTwo, operator) {
     } else if (operator === "/") {
         return divide(inputOne, inputTwo);
     };
-
 };
 let result = operate(inputOne, inputTwo, operator);
 console.log(result);
+
+//Below starts the code to make the display functional:
+let displayInput = document.getElementById("displayInput");
+displayInput.textContent = "";
+
+
+const numOne = document.querySelector("btn.num.one");
+numOne.addEventListener("click", () => {
+    displayInput.textContent = displayInput.textContent += 1;
+});
+const numTwo = document.querySelector("btn.num.two");
+numTwo.addEventListener("click", () => {
+    displayInput.textContent = displayInput.textContent += 2;
+});
+const numThree = document.querySelector("btn.num.three");
+numThree.addEventListener("click", () => {
+    displayInput.textContent = displayInput.textContent += 3;
+});
+const numFour = document.querySelector("btn.num.four");
+numFour.addEventListener("click", () => {
+    displayInput.textContent = displayInput.textContent += 4;
+});
+const numFive = document.querySelector("btn.num.five");
+numFive.addEventListener("click", () => {
+    displayInput.textContent = displayInput.textContent += 5;
+});
+const numSix = document.querySelector("btn.num.six");
+numSix.addEventListener("click", () => {
+    displayInput.textContent = displayInput.textContent += 6;
+});
+const numSeven = document.querySelector("btn.num.seven");
+numSeven.addEventListener("click", () => {
+    displayInput.textContent = displayInput.textContent += 7;
+});
+const numEight = document.querySelector("btn.num.eight");
+numEight.addEventListener("click", () => {
+    displayInput.textContent = displayInput.textContent += 8;
+});
+const numNine = document.querySelector("btn.num.nine");
+numNine.addEventListener("click", () => {
+    displayInput.textContent = displayInput.textContent += 9;
+});
+const numZero = document.querySelector("btn.num.zero");
+numZero.addEventListener("click", () => {
+    displayInput.textContent = displayInput.textContent += 0;
+});
+const funcDivide = document.querySelector("btn.func.divide");
+funcDivide.addEventListener("click", () => {
+    displayInput.textContent = displayInput.textContent += "/";
+});
+const funcMultiply = document.querySelector("btn.func.multiply");
+funcMultiply.addEventListener("click", () => {
+    displayInput.textContent = displayInput.textContent += "×";
+});
+const funcSubtract = document.querySelector("btn.func.subtract");
+funcSubtract.addEventListener("click", () => {
+    displayInput.textContent = displayInput.textContent += "-";
+});
+const funcAdd = document.querySelector("btn.func.add");
+funcAdd.addEventListener("click", () => {
+    displayInput.textContent = displayInput.textContent += "+";
+});
+const dotBtn = document.querySelector("btn.dot");
+dotBtn.addEventListener("click", () => {
+    displayInput.textContent = displayInput.textContent += ".";
+});
+const clearBtn = document.querySelector("btn.clear");
+clearBtn.addEventListener("click", () => {
+    displayInput.textContent = "";
+});
+
